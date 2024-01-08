631a61dca7a1ef43b8044a125f24c6418c7a4d54
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-01-07 20:48:31 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuVisible = false;
+
+const toggleShowMenu = () => {
+    if (menuVisible) {
+        select();
+    } else {
+        document.getElementById('nav').classList = 'responsive';
+        menuVisible = true;
+    }
+}
+
+const select = () => {
+    document.getElementById('nav').classList = '';
+    menuVisible = false;
+}
